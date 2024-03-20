620770323e7d7ee5285cf1abb807b41aacc7cad0
Mikael <mikael@Mini-de-Mikael.bbrouter>
2024-03-20 20:03:30 -0300

    criando sidebar

--- src/app/core/painel/painel/painel.component.ts
+++ src/app/core/painel/painel/painel.component.ts
@@ -7,5 +7,41 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./painel.component.scss']
 })
 export class PainelComponent {
-  
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.sidebar();
+  }
+
+  sidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#btn");
+    const searchBtn = document.querySelector(".bx-search");
+
+    if (sidebar) {
+      closeBtn?.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+        this.menuBtnChange();
+      });
+
+      searchBtn?.addEventListener("click", () => {
+        sidebar.classList.toggle("open");
+        this.menuBtnChange();
+      });
+    }
+  }
+
+  menuBtnChange() {
+    const sidebar = document.querySelector(".sidebar");
+    const closeBtn = document.querySelector("#btn");
+
+    if (sidebar && closeBtn) {
+      if (sidebar.classList.contains("open")) {
+        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+      } else {
+        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+      }
+    }
+  }
 }
