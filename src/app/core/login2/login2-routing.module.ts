51f6ea7529603dd725f8321377cc5596d30f01c5
mikaelangelo12 <mikaelangelo12@gmail.com>
2024-04-05 22:57:44 -0300

    instalando tailwind

--- src/app/core/login2/login2-routing.module.ts
+++ src/app/core/login2/login2-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login2Component } from './login2.component';
+import { AcessarContaComponent } from './components/acessar-conta/acessar-conta.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: Login2Component,
+    component: AcessarContaComponent,
   },
 ];
 
